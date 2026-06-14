05cd8b2bc12aad23ecc6a2ec97daaf0ca248f4cf
MashrurNafi <mashrurnafi2k2@gmail.com>
2026-06-15 05:32:12 +0600

    Add data to Users, Matches, and Bookings
    Inserted initial data for Users, Matches, and Bookings tables.

--- Query.sql
+++ Query.sql
@@ -35,3 +35,92 @@ CREATE TABLE bookings (
     payment_status varchar(25) check (payment_status in ('Confirmed', 'Pending', 'Canceled', 'Refunded')),
     total_cost int check(total_cost >= 0)
 );
+
+
+INSERT INTO
+    Users (user_id, full_name, email, role, phone_number)
+VALUES
+    (
+        1,
+        'Tanvir Rahman',
+        'tanvir@mail.com',
+        'Football Fan',
+        '+8801711111111'
+    ),
+    (
+        2,
+        'Asif Haque',
+        'asif@mail.com',
+        'Football Fan',
+        '+8801722222222'
+    ),
+    (
+        3,
+        'Sajjad Rahman',
+        'sajjad@mail.com',
+        'Ticket Manager',
+        '+8801733333333'
+    ),
+    (
+        4,
+        'Jannat Ara',
+        'jannat@mail.com',
+        'Football Fan',
+        NULL
+    );
+
+
+INSERT INTO
+    Matches (
+        fixture,
+        tournament_category,
+        base_ticket_price,
+        match_status
+    )
+VALUES
+    (
+        'Real Madrid vs Barcelona',
+        'Champions League',
+        150.00,
+        'Available'
+    ),
+    ( 
+        'Man City vs Liverpool',
+        'Premier League',
+        120.00,
+        'Selling Fast'
+    ),
+    (        
+        'Bayern Munich vs PSG',
+        'Champions League',
+        130.00,
+        'Available'
+    ),
+    (       
+        'AC Milan vs Inter Milan',
+        'Serie A',
+        90.00,
+        'Sold Out'
+    ),
+    (      
+        'Juventus vs Roma',
+        'Serie A',
+        80.00,
+        'Available'
+    );
+
+
+INSERT INTO
+    Bookings (
+        user_id,
+        match_id,
+        seat_number,
+        payment_status,
+        total_cost
+    )
+VALUES
+    (1, 101, 'A-12', 'Confirmed', 150.00),
+    (1, 102, 'B-04', 'Confirmed', 120.00),
+    (2, 101, 'A-13', 'Confirmed', 150.00),
+    (2, 101, NULL, NULL, 150.00),
+    (3, 102, 'C-20', 'Pending', 120.00);
